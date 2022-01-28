07de31c34d958720b8dc2a4d4c99c9694f27e3d7
Jessica Kitchen <jess@Jessicas-Mac-mini.local>
2022-01-27 21:06:32 -0500

    completed tasks for PrivateRoute and App Routing

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,9 @@ import styled from 'styled-components';
 import Header from './Header';
 import BloomHeader from './BloomHeader';
 import Login from './Login';
+import PrivateRoute from './PrivateRoute';
+import View from './View';
+import Logout from './Logout';
 
 const App = () => {
   return (
@@ -12,6 +15,19 @@ const App = () => {
       <BloomHeader/>
       <Header/>
       <RouteContainer>
+
+      <PrivateRoute exact path="/logout">
+          <Logout/>
+        </PrivateRoute>
+
+        <PrivateRoute exact path="/view">
+          <View/>
+        </PrivateRoute>
+
+      <Route exact path="/login">
+          <Login/>
+        </Route>
+
         <Route exact path="/">
           <Login/>
         </Route>          
@@ -21,12 +37,6 @@ const App = () => {
 }
 
 export default App;
-
-//Task List
-//1. Create and import PrivateRoute component.
-//2. Create a Route for Login pointing to '/login.'
-//3. Create a PrivateRoute for View component point to '/view.'
-//4. Create a PrivateRoute for Logout component pointing to '/logout.'
 
 
 const AppContainer = styled.div`
